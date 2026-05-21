9f7de408fe3e3ea6cffcaf3f1e04fc2700971cbe
kyumin <kyumin1429@naver.com>
2026-05-21 23:28:16 +0900

    change Icon

--- src/components/Layout/header.jsx
+++ src/components/Layout/header.jsx
@@ -1,11 +1,14 @@
 import './header.css'
-
+import sun from '../../../public/weather.svg'
 function Header() {
   return (
     <header>
 		
 	  	<ul>
-			<li className="logo">🌤️ SkyCast</li>
+			<li className="logo">
+				<img src={sun} alt="Sun" />
+				SkyCast
+			</li>
         	<li className='urls'>Home</li>
         	<li className='urls'>frequently-city</li>
         	<li className='urls'>week-weather</li>
