ea05ed329daad46420c9e3bf0c28f9317018c7b3
kyumin <kyumin1429@naver.com>
2026-05-21 13:07:02 +0900

    layout-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
+import Header from './components/Layout/header.jsx'
 
 function App() {
 
   return (
     <>
-	    
+	    <Header />
     </>
   );
 }
---
+++ src/components/Layout/header.jsx
@@ -0,0 +1,8 @@
+
+function Header() {
+	return (
+		<header>Header section</header>
+	);
+};
+
+export default Header;
