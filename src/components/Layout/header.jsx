d3af179d402eb5daeb0dc01547f154f7e97b823b
kyumin <kyumin1429@naver.com>
2026-06-04 12:21:52 +0900

    router make 2

--- src/components/Layout/header.jsx
+++ src/components/Layout/header.jsx
@@ -1,38 +1,11 @@
 import './header.css';
-
-function Header({ currentView, onViewChange }) {
+import { Link } from 'react-router-dom';
+function Header( ) {
   return (
     <header>
-        <ul>
-            <li className="logo" onClick={() => onViewChange('weather')} style={{ cursor: 'pointer' }}>
-                <img src="/weather.svg" alt="Sun" />
-                SkyCast
-            </li>
-            {/* Home 클릭 시 'weather' 뷰로 전환 */}
-            <li 
-              className={`urls1 ${currentView === 'weather' ? 'active' : ''}`}
-              onClick={() => onViewChange('weather')}
-              style={{ cursor: 'pointer' }}
-            >
-              Home
-            </li>
-            {/* frequently-city 클릭 시 'cities' 뷰로 전환 */}
-            <li 
-              className={`urls2 ${currentView === 'cities' ? 'active' : ''}`}
-              onClick={() => onViewChange('cities')}
-              style={{ cursor: 'pointer' }}
-            >
-              frequently-city
-            </li>
-            {/* week-weather 클릭 시 'week' 뷰로 전환 */}
-            <li 
-              className={`urls3 ${currentView === 'week' ? 'active' : ''}`}
-              onClick={() => onViewChange('week')}
-              style={{ cursor: 'pointer' }}
-            >
-              week-weather
-            </li>
-        </ul>
+        <Link to="/">홈</Link>
+        <Link to="/cities">자주 찾는 도시</Link>
+        <Link to="/week">주간 날씨</Link>
     </header>
   );
 }
