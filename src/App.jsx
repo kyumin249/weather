91544d3d430dbfaccf8a10027219f94a2b435512
kyumin <kyumin1429@naver.com>
2026-06-04 12:19:35 +0900

    router make

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Layout/header';
 import Main from './components/Layout/main';
+import Home from './pages/Home';
+import Cities from './pages/Cities';
+import Week from './pages/Week';
 
 function App() {
-  // 'weather' (Home), 'cities' (frequently-city), 'week' (week-weather) 화면 제어
-  const [view, setView] = useState('weather');
-
   return (
-    <div>
-      {/* Header에 현재 view 상태와 상태를 바꿀 함수를 넘겨줍니다 */}
-      <Header currentView={view} onViewChange={setView} />
-      {/* Main도 상위의 view 상태를 바라보게 합니다 */}
-      <Main view={view} onViewChange={setView} />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cities" element={<Cities />} />
+          <Route path="/week" element={<Week />} />
+        </Routes>
+      </Main>
+    </BrowserRouter>
   );
 }
 
