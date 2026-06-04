4297bd41a563d5f3cba0d7a9e47a33e51c270b19
kyumin <kyumin1429@naver.com>
2026-06-04 15:07:51 +0900

    test7

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/weather': {
-        target: 'https://apihub.kma.go.kr',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/weather/, '')
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api/weather': {
+  //       target: 'https://apihub.kma.go.kr',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api\/weather/, '')
+  //     }
+  //   }
+  // }
 });
